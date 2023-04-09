e67ef103c826305daf436e4c200050189085be1c
Riddikuluz <francisco.robledov@utem.cl>
2023-04-09 00:35:58 -0400

    ADD RUFFINI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstdlib>
 #include <string.h>
+#include <vector>
+#include <math.h>
 
 #define MAX 64
 using namespace std;
@@ -13,45 +15,38 @@ using namespace std;
 // todo[n][0] -> coeficiente
 // todo[n][1] -> grado
 // todo[n][2] -> negativo
-template<typename T, size_t n>
-void print_array(T const(&arr)[n]) {
-    for (size_t i = 0; i < n; i++) {
-        std::cout << arr[i] << ' ';
-    }
-    cout << endl;
-}
 
 int main(int argc, char *argv[]) {
     char *token = strtok(argv[1], " ");
     char *terminos[MAX];
     int todo[MAX][3], cont = 0;
 
-    for (int i = 0; i < MAX; i++) {
+    for (int i = 0; i < MAX; i++) {//inicializa la matriz
         for (int j = 0; j < 3; j++) {
             todo[i][j] = 0;
         }
     }
-    while (token != NULL) {
+
+    while (token != NULL) {//Obliviate
         terminos[cont] = token;
         token = strtok(NULL, " ");
         cont++;
     }
-
-    int con = 0;
+    int con = 0;//N de terminos
     for (int i = 0; i < cont; i++) {//malo
         token = strtok(terminos[i], "**");
         int d = 0;
         if (token != NULL) {
             while (token != NULL) {
                 todo[con][d] = abs(atoi(token));
-                if (*token == 88 || *token ==120){
-                    todo[con][0]=1;
-                    todo[con][1]=1;
+                if (*token == 88 || *token == 120) {
+                    todo[con][0] = 1;
+                    todo[con][1] = 1;
                 }
-                if (*token == 45 ) {
+                if (*token == 45) {
                     todo[con][2] = 1;
                 }
-                if (*token == 43 || *token == 45&& i != 0)
+                if (*token == 43 || *token == 45 && i != 0)
                     con--;
                 token = strtok(NULL, "x**");
                 d++;
@@ -59,18 +54,48 @@ int main(int argc, char *argv[]) {
             con++;
         }
     }
-/*Pruebas*/
-    print_array(todo[0]);
-    print_array(todo[1]);
-    print_array(todo[2]);
-    print_array(todo[3]);
-    print_array(todo[4]);
-    print_array(todo[5]);
-    print_array(todo[6]);
-    print_array(todo[7]);
-    print_array(todo[8]);
-    print_array(todo[9]);
-    print_array(todo[10]);
 
+    for (int i = 0; i < con; i++) {//Revisa si el coef deberia ser negativo
+        if (todo[i][2])
+            todo[i][0] = todo[i][0] * -1;
+        cout << " " << todo[i][0] << " " << "X ^" << todo[i][1] << " "
+             << endl;;//muestra el arreglo con los coefs y grados
+    }
+
+    int gra = 0;// Grado del polinomio
+    for (int i = 0; i < con; i++) {
+        if (gra < todo[i][1])
+            gra = todo[i][1];
+    }
+    //cout << "Grado: " << gra << endl;
+
+    int consta = 0;// Constante
+    for (int i = 0; i < con; i++) {
+        if (todo[i][1] == 0)
+            consta = todo[i][0];
+    }
+    //cout << "Constante: " << consta << endl;
+
+    //ruffini
+    vector<int> div_consta;
+    vector<int> Roots;
+    if (consta != 0) {
+        for (int i = 1; i <= abs(consta); i++) {
+            if (abs(consta) % i == 0) {
+                div_consta.push_back(i);
+                div_consta.push_back(-i);
+            }
+        }
+        for (int i = 0; i < div_consta.size(); i++){
+            int aux=0;
+            for (int j = 0; j < con; j++) {
+                aux=aux+todo[j][0]*pow(div_consta[i], todo[j][1]);
+            }
+            if(aux==0)
+            Roots.push_back(div_consta[i]);
+        }
+        for (int i = 0; i < Roots.size(); i++)
+            cout << "Root: " << Roots[i] << endl;
+    }
     return 0;
 }
